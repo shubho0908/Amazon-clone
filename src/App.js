cb867c942f5f6dfeefb0b3296cc2eac6236e113b
Shubhojeet Bera <shubhobera98@gmail.com>
2023-02-28 15:39:51 +0530

    project updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Navbar from "./Components/Navbar";
 import Home from "./Components/Home";
 import Men from "./Components/Category/Men";
 import Women from "./Components/Category/Women";
@@ -10,6 +11,7 @@ function App() {
   return (
     <>
       <BrowserRouter>
+      <Navbar/>
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route exact path="/men" element={<Men />} />
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import Logo from '../imgs/logo.png'
+import search from '../imgs/search.png'
+import './navbar.css'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <>
+    <div className="navbar">
+      <div className="left-section">
+          <img src={Logo} className="logo" />
+        <div className="search-bar">
+          <input type="text" className="search-box" placeholder='Search...'/>
+          <button className="search-btn">
+            <img src={search} className="search-img" />
+          </button>
+        </div>
+      </div>
+      <div className="right-section"></div>
+    </div>
+    </>
   )
 }
 
