3f3d8ace9038157e8c249a36c02b3e678a6bd51e
Shubhojeet Bera <shubhobera98@gmail.com>
2023-03-01 18:39:26 +0530

    project updated

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,19 +1,25 @@
-import React from 'react'
-import './home.css'
+import React from "react";
+import "./home.css";
+import Delivery from "../imgs/delivery.png";
 
 function Home() {
   return (
     <>
-    <div className="App">
-    <div className="poster-area">
-      <div className="poster-data">
-      <p className="poster-head">Free Delivery!</p>
-      <p className="poster-desc">Don't miss it out! Only today, get free Next Day delivery on all your orders.</p>
+      <div className="App">
+        <div className="poster-area">
+          <div className="poster-data">
+            <p className="poster-head">Free Delivery!</p>
+            <p className="poster-desc">
+              Don't miss it out! Only today, get free Next Day delivery on all
+              your orders.
+            </p>
+          </div>
+          <button className="browse-btn">Browse products</button>
+        </div>
+        <img src={Delivery} className="delivery" />
       </div>
-    </div>
-    </div>
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
