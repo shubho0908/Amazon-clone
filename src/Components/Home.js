55747dd1d94dcdd4420c6d6384cc8e985457ad22
Shubhojeet Bera <shubhobera98@gmail.com>
2023-03-01 17:10:58 +0530

    project updated

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import './home.css'
 
 function Home() {
   return (
     <>
     <div className="App">
-    Home
+    <div className="poster-area">
+      <div className="poster-data">
+      <p className="poster-head">Free Delivery!</p>
+      <p className="poster-desc">Don't miss it out! Only today, get free Next Day delivery on all your orders.</p>
+      </div>
+    </div>
     </div>
     </>
   )
