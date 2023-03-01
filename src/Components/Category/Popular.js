929463892d41af6f48e33af4be9b054827a695df
Shubhojeet Bera <shubhobera98@gmail.com>
2023-03-01 21:35:11 +0530

    project updated

---
+++ src/Components/Category/Popular.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import "./popular.css";
+
+// IMAGES
+
+import Men from "./Img/men.png";
+import Women from "./Img/women.png";
+import Jwellery from "./Img/jwelery.png";
+import Electronics from "./Img/pc.png";
+import MenWhite from "./Img/men-white.png";
+import WomenWhite from "./Img/women-white.png";
+import JwelleryWhite from "./Img/jwelery-white.png";
+import ElectronicsWhite from "./Img/pc-white.png";
+
+function Popular() {
+  return (
+    <>
+      <div className="popular">
+        <div className="popular-data">
+          <p className="popular-head">Popular categories</p>
+        </div>
+        <div className="popular-items">
+          <img src={Electronics} className="electronics" />
+          <img src={Jwellery} className="jwellery" />
+          <img src={Men} className="men" />
+          <img src={Women} className="women" />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Popular;
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./home.css";
 import Delivery from "../imgs/delivery.png";
+import Popular from "./Category/Popular";
 
 function Home() {
   return (
@@ -10,13 +11,14 @@ function Home() {
           <div className="poster-data">
             <p className="poster-head">Free Delivery!</p>
             <p className="poster-desc">
-              Don't miss it out! Only today, get free Next Day delivery on all
+              Don't miss it out! Only today, get free <b style={{fontSize:"22px"}}>Next Day</b> delivery on all
               your orders.
             </p>
           </div>
           <button className="browse-btn">Browse products</button>
         </div>
         <img src={Delivery} className="delivery" />
+        <Popular/>
       </div>
     </>
   );
