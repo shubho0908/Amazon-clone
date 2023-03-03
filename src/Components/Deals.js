ae18bc2bc64335ca1b52d04fd848520c117cdd39
Shubhojeet Bera <shubhobera98@gmail.com>
2023-03-03 14:30:17 +0530

    project updated

--- src/Components/Deals.js
+++ src/Components/Deals.js
@@ -2,6 +2,7 @@ import { React, useEffect, useState } from "react";
 import "./deals.css";
 import Add from "../imgs/heart.png";
 import Added from "../imgs/red-heart.png";
+import rating from "../imgs/rating.png";
 
 function Deals() {
   const [AllProducts, setAllProducts] = useState([]);
@@ -12,6 +13,12 @@ function Deals() {
       const data = await fetch("https://fakestoreapi.com/products");
       const new_data = await data.json();
       setAllProducts(new_data);
+      // Add a review number property to each item object
+      const productsWithReviewNumber = new_data.map((item) => ({
+        ...item,
+        reviewNumber: Math.floor(Math.random() * (150 - 50 + 1)) + 50,
+      }));
+      setAllProducts(productsWithReviewNumber);
     };
 
     GetProducts();
@@ -27,9 +34,13 @@ function Deals() {
               <div className="card" key={items.id}>
                 <div className="card-img-data">
                   <img src={items.image} className="card-img" />
-                  <img onClick={() => {
-                    setListadded(!Listadded)
-                  }} src={Listadded ? Added : Add} className="add-list" />
+                  <img
+                    onClick={() => {
+                      setListadded(!Listadded);
+                    }}
+                    src={Listadded ? Added : Add}
+                    className="add-list"
+                  />
                   <button className="view">View product</button>
                 </div>
                 <div className="card-data">
@@ -38,7 +49,19 @@ function Deals() {
                       ? items.title.slice(0, 32) + ".."
                       : items.title}
                   </p>
-                  <p className="card-category">{items.category}</p>
+                  <div className="category-rating">
+                    <p className="card-category">{items.category}</p>
+                    <div className="rating">
+                      <img src={rating} className="rating-img" />
+                      <img src={rating} className="rating-img" />
+                      <img src={rating} className="rating-img" />
+                      <img src={rating} className="rating-img" />
+                      <img src={rating} className="rating-img" />
+                      <p className="rating-text">
+                        {"5 " + "(" + items.reviewNumber + " reviews)"}
+                      </p>
+                    </div>
+                  </div>
                   <div className="card-price">
                     <p className="discount">${items.price}</p>
                     <p className="mrp">${Math.round(items.price * 1.66)}</p>
