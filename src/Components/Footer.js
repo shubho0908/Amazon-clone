f491e522645399759781b78458be915daebd29c8
Shubhojeet Bera <shubhobera98@gmail.com>
2023-03-04 18:23:06 +0530

    project updated

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -11,7 +11,30 @@ function Footer() {
       </div>
     </div>
     <div className="link-section">
-      
+      <div className="first-row">
+        <p className='bold'>Get to Know Us</p>
+        <p>Make Money with Us</p>
+        <p>Amazon Payment</p>
+        <p>Let Us Help You</p>
+      </div>
+      <div className="second-row">
+        <p className='bold'>About Amazon</p>
+        <p>Sell products on Amazon</p>
+        <p>Amazon Business Card</p>
+        <p>Amazon and COVID-19</p>
+      </div>
+      <div className="third-row">
+        <p className='bold'>Connect with Us</p>
+        <p>Sell apps on Amazon</p>
+        <p>Shop with Points</p>
+        <p>Shipping Rates & Policies</p>
+      </div>
+      <div className="fourth-row">
+        <p className='bold'>Amazon Cares</p>
+        <p>Become an Affiliate</p>
+        <p>Reload Your Balance</p>
+        <p>Returns & Replacements</p>
+      </div>
     </div>
     </>
   )
