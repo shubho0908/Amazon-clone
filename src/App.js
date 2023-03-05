bfc1fa8826a46c8804dbf488cc2cb47e3ced6199
Shubhojeet Bera <shubhobera98@gmail.com>
2023-03-05 15:45:42 +0530

    auth included

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import "./App.css";
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Routes, Route, BrowserRouter, Navigate } from "react-router-dom"; // Import the Navigate component from react-router-dom
 import Home from "./Components/Home";
 import Men from "./Components/Category/Men";
 import Women from "./Components/Category/Women";
 import Kids from "./Components/Category/Electronics";
 import Lists from "./Components/Lists";
 import Signin from "./Components/Signin";
+import Signup from "./Components/Signup";
+import { app } from "./Firebase";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+const auth = getAuth(app);
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+      } else {
+        setUser(null);
+      }
+    });
+  }, []);
+
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route exact path="/" element={<Signin />} />
-          <Route exact path="/home" element={<Home />} />
-          <Route exact path="/men" element={<Men />} />
-          <Route exact path="/women" element={<Women />} />
-          <Route exact path="/kids" element={<Kids />} />
-          <Route exact path="/wishlists" element={<Lists />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <Routes>
+        {user === null ? (
+          <>
+            <Route exact path="/" element={<Signin />} />
+            <Route exact path="/signup" element={<Signup />} />
+          </>
+        ) : (
+          <>
+            <Route exact path="/home" element={<Home />} />
+            <Route exact path="/men" element={<Men />} />
+            <Route exact path="/women" element={<Women />} />
+            <Route exact path="/kids" element={<Kids />} />
+            <Route exact path="/wishlists" element={<Lists />} />
+          </>
+        )}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -4,27 +4,31 @@ import Logo from "../imgs/logo2.png";
 import BG1 from "../imgs/login-BG.png";
 import BG2 from "../imgs/login-BG2.png";
 import google from "../imgs/google.png";
-import { Link, useNavigate } from "react-router-dom";
 import { app } from "../Firebase";
+import { Link, useNavigate } from "react-router-dom";
 import {
   getAuth,
-  signInWithEmailAndPassword,
-  GoogleAuthProvider,
-  signInWithPopup,
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider, signInWithPopup
 } from "firebase/auth";
 
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 
-function Signin() {
+function Signup() {
   const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
   const [password, setPassword] = useState("");
   const [emailError, setEmailError] = useState("");
   const [PasswordError, setPasswordError] = useState("");
-  const navigate = useNavigate();
 
+  const navigate = useNavigate();
   const handleEmailChange = (event) => {
     setEmail(event.target.value);
+  };
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
   };
 
   const handlePasswordChange = (event) => {
@@ -53,11 +57,12 @@ function Signin() {
     }
   };
 
-  const LogInUser = async () => {
-    signInWithEmailAndPassword(auth, email, password)
+  const CreateUser = async () => {
+    createUserWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
         const user = userCredential.user;
         console.log(user);
+        navigate("/home");
       })
       .catch((error) => {
         console.log(error);
@@ -87,8 +92,8 @@ function Signin() {
             <img src={Logo} className="amazon-logo" />
           </div>
           <div className="signup">
-            <Link to="/signup">
-              <button className="signup-btn">Sign up</button>
+            <Link to="/">
+              <button className="signup-btn">Sign in</button>
             </Link>
           </div>
         </div>
@@ -99,12 +104,20 @@ function Signin() {
         <div className="main-form">
           <div className="login-form">
             <div className="some-text">
-              <p className="user">User Login</p>
+              <p className="user">User Registration</p>
               <p className="user-desc">
-                Hey, Enter your details to get sign in to your account
+                Hey, Enter your details to create a new account
               </p>
             </div>
             <div className="user-details">
+              <input
+                type="text"
+                placeholder="Name"
+                className="name"
+                value={name}
+                onChange={handleNameChange}
+                required
+              />
               <input
                 type="email"
                 placeholder="Enter Email"
@@ -128,13 +141,13 @@ function Signin() {
               {PasswordError && (
                 <div className="error-message">{PasswordError}</div>
               )}
-              <button onClick={LogInUser} className="signin-btn">
-                Sign in
+              <button onClick={CreateUser} className="signin-btn">
+                Sign up
               </button>
               <div className="extra-buttons">
                 <p className="or">&#x2015; Or &#x2015;</p>
                 <button onClick={GoogleAuth} className="google">
-                  <p>Sign in with</p>
+                  <p>Sign up with</p>
                   <img src={google} className="google-img" />
                 </button>
               </div>
@@ -146,4 +159,4 @@ function Signin() {
   );
 }
 
-export default Signin;
+export default Signup;
