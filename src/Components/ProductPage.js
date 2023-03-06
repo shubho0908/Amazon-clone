255a0059ab4f31e3440e892468cedc4c67b10af1
Shubhojeet Bera <shubhobera98@gmail.com>
2023-03-06 11:54:31 +0530

    project updated

--- src/Components/ProductPage.js
+++ src/Components/ProductPage.js
@@ -1,23 +1,39 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import Navbar from "./Navbar";
+import './productpage.css'
 
 function ProductPage() {
   const { id } = useParams();
   const [product, setProduct] = useState("");
-  
+
   useEffect(() => {
     const getProducts = async () => {
       const data = await fetch(`https://fakestoreapi.com/products/${id}`);
       const new_data = await data.json();
       setProduct(new_data);
+      console.log(product);
     };
     getProducts();
   }, []);
 
-  useEffect(() => {
-  }, [product]);
+  return (
+    <>
+    <Navbar/>
+      <div className="product-page">
+        <div className="product-data">
+          <div className="item-image">
+          <img src={product.image} className={`item-img ${product.image ? 'img-style' : ''}`} />
 
-  return <div>ProductPage</div>;
+          </div>
+          <div className="product-details">
+            <p className="item-title">{product.title}</p>
+            <p className="item-desc">{product.description}</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default ProductPage;
