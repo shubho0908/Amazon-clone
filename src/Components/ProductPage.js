c900f1b069eeb1b017cc3ca5ad927d05378621b8
Shubhojeet Bera <shubhobera98@gmail.com>
2023-03-06 12:35:57 +0530

    project updated

--- src/Components/ProductPage.js
+++ src/Components/ProductPage.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import Navbar from "./Navbar";
-import Footer from './Footer'
-import './productpage.css'
+import Footer from "./Footer";
+import "./productpage.css";
+import Rating from "../imgs/rating.png";
 
 function ProductPage() {
   const { id } = useParams();
@@ -13,27 +14,35 @@ function ProductPage() {
       const data = await fetch(`https://fakestoreapi.com/products/${id}`);
       const new_data = await data.json();
       setProduct(new_data);
-      console.log(product);
     };
     getProducts();
   }, []);
 
   return (
     <>
-    <Navbar/>
+      <Navbar />
       <div className="product-page">
         <div className="product-data">
           <div className="item-image">
-          <img src={product.image} className={`item-img ${product.image ? 'img-style' : ''}`} />
-
+            <img
+              src={product.image}
+              className={`item-img ${product.image ? "img-style" : ""}`}
+            />
           </div>
           <div className="product-details">
             <p className="item-title">{product.title}</p>
             <p className="item-desc">{product.description}</p>
+            <div className="item-rating">
+              <img src={Rating} className="rating-img" />
+              <img src={Rating} className="rating-img" />
+              <img src={Rating} className="rating-img" />
+              <img src={Rating} className="rating-img" />
+              <img src={Rating} className="rating-img" />
+            </div>
           </div>
         </div>
       </div>
-        <Footer/>
+      <Footer />
     </>
   );
 }
